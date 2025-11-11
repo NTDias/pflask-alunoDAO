5b424b17f0af4b3110320c17d98612b973372192
Netto Dias <pereirasecinfo@gmail.com>
2025-11-11 20:07:38 -0300

    config

--- dao/db_config.py
+++ dao/db_config.py
@@ -1,6 +1,5 @@
 import sqlite3
 import psycopg2
-# path / url de conexão
 DB_PATH = "postgresql://neondb_owner:npg_EWpC7N5KmoXz@ep-lucky-forest-acjbx4jh-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require"
 def get_connection():
     return psycopg2.connect(DB_PATH)
